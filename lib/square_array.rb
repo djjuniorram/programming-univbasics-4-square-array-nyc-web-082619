337241fc954d6edf45340522fec8d4a5be0b9c6d
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-08-21 03:32:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,7 @@
-def square_array(array)
-  new_array = []
-  array.each{|a| new_array.push(a*a)}
-  return new_array
+def square_array(numbers)
+  # your code here	  new_numbers = []
+end 	  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
 end
